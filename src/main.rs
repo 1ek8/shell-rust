3ee45e71010c3bafe59bb8f3e9989118912b6f6d
Aniket <aniketsingh2151@gmail.com>
2025-11-28 21:58:06 +0530

    add type command, create list of commands for lookup

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     loop {
-            print!("$ ");
+        
+        print!("$ ");
         io::stdout().flush().unwrap();
         // thread::sleep(Duration::from_secs(3));
 
@@ -16,8 +17,17 @@ fn main() {
         let parts: Vec<&str> = input.trim().split_whitespace().collect();
         let command = parts[0];
         let args = &parts[1..];
-
+        
+        const BUILT_INS: [&str;3] = ["exit", "echo", "type"];
         match command {
+            "type" => {
+                if BUILT_INS.contains(&args[0]) {
+                    println!("{} is a shell builtin", args[0])
+                } else {
+                    println!("{}: not found", args[0]);
+
+                }
+            },
             "exit" => {
                 break;
             },
