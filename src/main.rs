fced8e9ca280dda0141b309c68d3ac66dc902a19
Aniket <aniketsingh2151@gmail.com>
2025-11-28 13:35:54 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+// use std::thread;
+// use std::time::Duration;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    // thread::sleep(Duration::from_secs(3));
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}, command not found", command.trim());
 }
